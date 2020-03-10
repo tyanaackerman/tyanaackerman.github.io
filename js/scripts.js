bed2aa58e57ed6c93a6154569086a443fd60585a
tyanaackerman <60484066+tyanaackerman@users.noreply.github.com>
2020-03-09 20:48:35 -0700

    Updating CSS, HTML, & JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log('OK');
+
+var button = document.querySelector('button');
+var body = document.querySelector('body');
+
+button.addEventListener(  'click' , () => {
+  
+  body.classList.toggle('dark')
+  console.log(  'okey dokey'  );
+  
+}  );
