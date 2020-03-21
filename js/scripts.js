e66688648db03d629e57c799f29039b6467e64da
tyanaackerman <60484066+tyanaackerman@users.noreply.github.com>
2020-03-20 18:13:12 -0700

    Adding Changes Made in CodePen
    YAY!!!

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-console.log('OK');
+// console.log('OK');
 
-var button = document.querySelector('button');
-var body = document.querySelector('body');
+// var button = document.querySelector('button');
+// var body = document.querySelector('body');
 
-button.addEventListener(  'click' , () => {
+// button.addEventListener(  'click' , () => {
   
-  body.classList.toggle('dark')
-  console.log(  'okey dokey'  );
+//   body.classList.toggle('dark')
+//   console.log(  'okey dokey'  );
   
-}  );
+// }  );
